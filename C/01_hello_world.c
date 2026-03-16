b9d6a8ba086a92b77bc3e7bba2e2b60bf07e244c
Aswin Prakash <aswinputhiyaparambil@gmail.com>
2026-03-16 20:34:18 +0530

    Enhance comments and structure in hello world program
    Added comments and improved formatting in the hello world program.

---
+++ C/01_hello_world.c
@@ -0,0 +1,35 @@
+#include <stdio.h>   // allows input and output functions like printf
+
+int main()           // main function: program starts running from here
+{
+    printf("Hello World\n");   // prints text to the screen
+
+    return 0;       // indicates program finished successfully
+}
+
+
+
+/*
+Important Things to Notice
+1. #include <stdio.h>
+Allows the program to use input/output functions.
+
+2. int main()
+The starting point of every C program.
+
+3. printf()
+Used to display text on the screen.
+
+4. return 0
+Ends the program properly.
+
+
+
+Logic Thinking
+Even a simple program follows logic:
+
+Start program
+Print message
+End program
+
+*/
